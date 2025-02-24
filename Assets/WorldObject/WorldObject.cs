a50482577faadd573b1ab195071ebd2ce158278a
Zeng Fanfan <hurryzeng@foxmail.com>
2025-02-24 19:06:30 +0800

    Part 4: World Objects

---
+++ Assets/WorldObject/WorldObject.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class WorldObject : MonoBehaviour {
+    public string objectName;
+    public Texture2D buildImage;
+    public int cost, sellValue, hitPoints, maxHitPoints;
+    protected Player player;
+    protected string[] actions = { };
+    protected bool currentlySelected = false;
+    protected virtual void Awake() {
+    }
+
+    protected virtual void Start() {
+        player = transform.root.GetComponentInChildren<Player>();
+    }
+
+    protected virtual void Update() {
+    }
+
+    protected virtual void OnGUI() {
+    }
+
+    public void SetSelection(bool selected) {
+        currentlySelected = selected;
+    }
+
+    public string[] GetActions() {
+        return actions;
+    }
+
+    public virtual void PerformAction(string actionToPerform) {
+        //it is up to children with specific actions to determine what to do with each of those actions
+    }
+}

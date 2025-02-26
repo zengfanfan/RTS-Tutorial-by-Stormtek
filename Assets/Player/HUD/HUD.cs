d51e7343f33fdedd97128657024a319aa8caf76c
Zeng Fanfan <hurryzeng@foxmail.com>
2025-02-26 14:16:03 +0800

    Part 7: Custom Mouse Cursor

--- Assets/Player/HUD/HUD.cs
+++ Assets/Player/HUD/HUD.cs
@@ -2,19 +2,28 @@ using RTS;
 using UnityEngine;
 
 public class HUD : MonoBehaviour {
+    public GUISkin mouseCursorSkin;
     public GUISkin resourceSkin, ordersSkin, selectBoxSkin;
+    public Texture2D activeCursor;
+    public Texture2D selectCursor, leftCursor, rightCursor, upCursor, downCursor;
+    public Texture2D[] moveCursors, attackCursors, harvestCursors;
+    private CursorState activeCursorState;
+    private int currentFrame = 0;
+
     const int ORDERS_BAR_WIDTH = 150, RESOURCE_BAR_HEIGHT = 40;
     Player player;
 
     void Start() {
         player = transform.root.GetComponent<Player>();
         ResourceManager.StoreSelectBoxItems(selectBoxSkin);
+        SetCursorState(CursorState.Select);
     }
 
     void OnGUI() {
         if (player && player.human) {
             DrawOrdersBar();
             DrawResourceBar();
+            DrawMouseCursor();
         }
     }
 
@@ -51,6 +60,84 @@ public class HUD : MonoBehaviour {
         return insideWidth && insideHeight;
     }
 
-    public Rect GetPlayingArea() => new Rect(0, RESOURCE_BAR_HEIGHT, Screen.width - ORDERS_BAR_WIDTH, Screen.height - RESOURCE_BAR_HEIGHT);
+    public Rect GetPlayingArea() => new(0, RESOURCE_BAR_HEIGHT, Screen.width - ORDERS_BAR_WIDTH, Screen.height - RESOURCE_BAR_HEIGHT);
+
+    private void DrawMouseCursor() {
+        bool mouseOverHud = !MouseInBounds() && activeCursorState != CursorState.PanRight && activeCursorState != CursorState.PanUp;
+        if (mouseOverHud) {
+            Cursor.visible = true;
+        } else {
+            Cursor.visible = false;
+            GUI.skin = mouseCursorSkin;
+            GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
+            UpdateCursorAnimation();
+            Rect cursorPosition = GetCursorDrawPosition();
+            GUI.Label(cursorPosition, activeCursor);
+            GUI.EndGroup();
+        }
+    }
+
+    private void UpdateCursorAnimation() {
+        //sequence animation for cursor (based on more than one image for the cursor)
+        //change once per second, loops through array of images
+        if (activeCursorState == CursorState.Move) {
+            currentFrame = (int)Time.time % moveCursors.Length;
+            activeCursor = moveCursors[currentFrame];
+        } else if (activeCursorState == CursorState.Attack) {
+            currentFrame = (int)Time.time % attackCursors.Length;
+            activeCursor = attackCursors[currentFrame];
+        } else if (activeCursorState == CursorState.Harvest) {
+            currentFrame = (int)Time.time % harvestCursors.Length;
+            activeCursor = harvestCursors[currentFrame];
+        }
+    }
+
+    private Rect GetCursorDrawPosition() {
+        //set base position for custom cursor image
+        float leftPos = Input.mousePosition.x;
+        float topPos = Screen.height - Input.mousePosition.y; //screen draw coordinates are inverted
+                                                              //adjust position base on the type of cursor being shown
+        if (activeCursorState == CursorState.PanRight) leftPos = Screen.width - activeCursor.width;
+        else if (activeCursorState == CursorState.PanDown) topPos = Screen.height - activeCursor.height;
+        else if (activeCursorState == CursorState.Move || activeCursorState == CursorState.Select || activeCursorState == CursorState.Harvest) {
+            topPos -= activeCursor.height / 2;
+            leftPos -= activeCursor.width / 2;
+        }
+        return new(leftPos, topPos, activeCursor.width, activeCursor.height);
+    }
+
+    public void SetCursorState(CursorState newState) {
+        activeCursorState = newState;
+        switch (newState) {
+        case CursorState.Select:
+            activeCursor = selectCursor;
+            break;
+        case CursorState.Attack:
+            currentFrame = (int)Time.time % attackCursors.Length;
+            activeCursor = attackCursors[currentFrame];
+            break;
+        case CursorState.Harvest:
+            currentFrame = (int)Time.time % harvestCursors.Length;
+            activeCursor = harvestCursors[currentFrame];
+            break;
+        case CursorState.Move:
+            currentFrame = (int)Time.time % moveCursors.Length;
+            activeCursor = moveCursors[currentFrame];
+            break;
+        case CursorState.PanLeft:
+            activeCursor = leftCursor;
+            break;
+        case CursorState.PanRight:
+            activeCursor = rightCursor;
+            break;
+        case CursorState.PanUp:
+            activeCursor = upCursor;
+            break;
+        case CursorState.PanDown:
+            activeCursor = downCursor;
+            break;
+        default: break;
+        }
+    }
 
 }
--- Assets/Player/UserInput.cs
+++ Assets/Player/UserInput.cs
@@ -78,17 +78,26 @@ public class UserInput : MonoBehaviour {
         float xpos = Input.mousePosition.x;
         float ypos = Input.mousePosition.y;
         var movement = new Vector3(0, 0, 0);
+        bool mouseScroll = false;
         //horizontal camera movement
         if (xpos >= 0 && xpos < ResourceManager.ScrollWidth) {
             movement.x -= ResourceManager.ScrollSpeed;
+            player.hud.SetCursorState(CursorState.PanLeft);
+            mouseScroll = true;
         } else if (xpos <= Screen.width && xpos > Screen.width - ResourceManager.ScrollWidth) {
             movement.x += ResourceManager.ScrollSpeed;
+            player.hud.SetCursorState(CursorState.PanRight);
+            mouseScroll = true;
         }
         //vertical camera movement
         if (ypos >= 0 && ypos < ResourceManager.ScrollWidth) {
             movement.z -= ResourceManager.ScrollSpeed;
+            player.hud.SetCursorState(CursorState.PanDown);
+            mouseScroll = true;
         } else if (ypos <= Screen.height && ypos > Screen.height - ResourceManager.ScrollWidth) {
             movement.z += ResourceManager.ScrollSpeed;
+            player.hud.SetCursorState(CursorState.PanUp);
+            mouseScroll = true;
         }
         //make sure movement is in the direction the camera is pointing
         //but ignore the vertical tilt of the camera to get sensible scrolling
@@ -111,6 +120,10 @@ public class UserInput : MonoBehaviour {
         //if a change in position is detected perform the necessary update
         if (destination != origin) {
             Camera.main.transform.position = Vector3.MoveTowards(origin, destination, Time.deltaTime * ResourceManager.ScrollSpeed);
+        }
+
+        if (!mouseScroll) {
+            player.hud.SetCursorState(CursorState.Select);
         }
     }
 }
